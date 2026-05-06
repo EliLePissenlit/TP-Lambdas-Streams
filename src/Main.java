6ac7818470798a9399163dd2ab2a229da5d82930
EliLePissenlit <e.clement1002@gmail.com>
2026-05-06 10:19:05 +0200

    partie 1 + .class intellij + test main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 import factory.TripFactory;
 import models.Trip;
+import exo.Partie1;
 
-void main() {
-    List<Trip> trips = TripFactory.generateTrips(10000000);
+import java.util.List;
 
-    // appeler les méthodes des exos ici
-    // pour tester si ça marche bien, générer une liste de 10 éléments et afficher le résultat
+public class Main {
+
+
+    public static void main(String[] args) {
+        // test partie 1
+        List<Trip> trips = TripFactory.generateTrips(10);
+
+        Partie1 p1 = new Partie1();
+
+        List<Trip> r1 = p1.longAndExpensiveTrips(trips);
+        List<Trip> r2 = p1.badTrips(trips);
+        List<Trip> r3 = p1.recentTrips(trips);
+
+        System.out.println("nb trips=" + trips.size()
+                + " long+cher=" + r1.size()
+                + " mauvais=" + r2.size()
+                + " recent=" + r3.size());
+    }
 }
--- src/exo/Partie1.java
+++ src/exo/Partie1.java
@@ -2,22 +2,53 @@ package exo;
 
 import models.Trip;
 
+// pour date de today et yesterday
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Partie1 {
 
     public List<Trip> longAndExpensiveTrips(List<Trip> trips) {
         // distance > 10km et prix > 20€
-        return List.of();
+
+        // stream ok
+        return trips.stream()
+                .filter(t -> t.distanceKm() > 10)
+                .filter(t -> t.price() > 20)
+                .collect(Collectors.toList());
     }
 
     public List<Trip> badTrips(List<Trip> trips) {
         // rating < 3
-        return List.of();
+
+        //ok
+        return trips.stream()
+                .filter(t -> t.rating() < 3)
+                .collect(Collectors.toList());
     }
 
     public List<Trip> recentTrips(List<Trip> trips) {
         // aujourd’hui ou hier
-        return List.of();
+        LocalDate ajd = LocalDate.now();
+
+
+        //ocaldate now et minusDays pour enlever jour 
+
+        LocalDate hier = ajd.minusDays(1);
+
+        return trips.stream()
+                .filter(t -> {
+                    LocalDate d = t.startTime().toLocalDate();
+                    return d.equals(ajd) || d.equals(hier);
+                }
+            )
+
+         
+         
+         
+            .collect(Collectors.toList());
     }
+
+
 }
