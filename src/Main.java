d65675220ee1871f483750b7d47107fbc8ccbb4a
EliLePissenlit <e.clement1002@gmail.com>
2026-05-06 10:55:09 +0200

    partie 2 stat + main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import factory.TripFactory;
 import models.Trip;
 import exo.Partie1;
+import exo.Partie2;
 
 import java.util.List;
 
@@ -8,11 +9,11 @@ public class Main {
 
 
     public static void main(String[] args) {
-        // test partie 1
+        //10 traj)
         List<Trip> trips = TripFactory.generateTrips(10);
 
+        // p 1
         Partie1 p1 = new Partie1();
-
         List<Trip> r1 = p1.longAndExpensiveTrips(trips);
         List<Trip> r2 = p1.badTrips(trips);
         List<Trip> r3 = p1.recentTrips(trips);
@@ -21,5 +22,11 @@ public class Main {
                 + " long+cher=" + r1.size()
                 + " mauvais=" + r2.size()
                 + " recent=" + r3.size());
+
+        // ^p 2 stats
+        Partie2 p2 = new Partie2();
+        System.out.println("/ v=" + p2.countByCity(trips));
+        System.out.println("$ driver " + p2.revenueByDriver(trips));
+        System.out.println("tmp moy / ville=" + p2.avgDurationByCity(trips));
     }
 }
--- src/exo/Partie2.java
+++ src/exo/Partie2.java
@@ -5,22 +5,64 @@ import models.Trip;
 import java.util.List;
 import java.util.Map;
 
+
+
+import java.util.stream.Collectors;
+
 public class Partie2 {
 
     public Map<String, Long> countByCity(List<Trip> trips) {
-        // coder ici
-        return Map.of();
+        // nb trajets / ville
+        //donc groupingBy + counting // ok ?
+        return trips.stream()
+        
+                .collect(Collectors.groupingBy(
+
+                        t -> t.city(),
+
+                        
+                        Collectors.counting()
+                ))
+                ;
+
+
     }
 
     public Map<String, Double> revenueByDriver(List<Trip> trips) {
         // revenu par chauffeur
-        // coder ici
-        return Map.of();
+         //+prix / driver, collectors
+        return trips.stream()
+
+                .collect(Collectors.groupingBy(
+
+                        t -> t.driverId(),
+                        Collectors.summingDouble(t -> t.price())
+
+                        
+                )
+        );
     }
 
     public Map<String, Double> avgDurationByCity(List<Trip> trips) {
         // coder ici
         // durée moyenne par ville
-        return Map.of();
+        // averagingD -> durationM moy
+        return trips.stream()
+
+                .collect(Collectors.groupingBy(
+
+                        t -> t.city(),
+
+                        Collectors.averagingDouble(t -> t.durationMin())
+                
+             
+                )
+        )
+                ;
+
     }
+
+
 }
+
+
